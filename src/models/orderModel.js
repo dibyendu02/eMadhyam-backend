02af747007d898a81e6288a2043f7c0969c8429c
Dibyendu Das <get2dibyendu@gmail.com>
2025-02-03 20:56:36 +0530

    order routes update

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -6,11 +6,13 @@ const OrderSchema = new Schema({
   products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
   time: { type: Date, default: Date.now },
   isPaid: { type: Boolean, default: false },
+  paymentMethod: { type: String },
   status: {
     type: String,
     enum: ["pending", "processing", "delivered", "cancelled"],
     default: "pending",
   },
+  deliveryDate: { type: Date },
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
